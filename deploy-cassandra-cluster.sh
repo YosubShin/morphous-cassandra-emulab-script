8219924517fdbc679a3a403587396f86dce7c52b
Yosub Shin <yosub@yosubshin.com>
2014-08-23 11:58:36 -0500

    No need to add ssh username

--- deploy-cassandra-cluster.sh
+++ deploy-cassandra-cluster.sh
@@ -9,7 +9,6 @@ CASSANDRA_SRC_TAR_FILE=cassandra-src.tar.gz
 REMOTE_BASE_DIR=/scratch/ISS/shin14/repos
 REMOTE_SCRIPT_DIR=$REMOTE_BASE_DIR/morphous-cassandra-emulab-script
 REMOTE_REDEPLOY_SCRIPT=$REMOTE_SCRIPT_DIR/redeploy-node-script.sh
-SSH_USER=yossupp
 
 # Set JAVA_HOME to build with Ant
 export JAVA_HOME=/usr/lib/jvm/jdk1.7.0
@@ -41,13 +40,17 @@ for (( i=0; i < CLUSTER_SIZE; i++))
 do
 if [ $i == 0 ]; then
     echo "## Invoking redeploy script on node-$i"
-    ssh -o "StrictHostKeyChecking no" $SSH_USER@node-$i "sudo $REMOTE_REDEPLOY_SCRIPT node-$i"
+    ssh -o "StrictHostKeyChecking no" node-$i "sudo $REMOTE_REDEPLOY_SCRIPT node-$i"
     echo "## Finished invoking redeploy script on node-$i"
 else
     (
     echo "## Invoking redeploy script on node-$i"
-    ssh -o "StrictHostKeyChecking no" $SSH_USER@node-$i "sudo $REMOTE_REDEPLOY_SCRIPT node-$i"
+    ssh -o "StrictHostKeyChecking no" node-$i "sudo $REMOTE_REDEPLOY_SCRIPT node-$i"
     echo "## Finished invoking redeploy script on node-$i"
     ) &
 fi
 done
+
+wait
+echo "## Finished invoking redeploy scripts on all nodes."
+
