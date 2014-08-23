da98381bb60c9b9706f2a291e4e5dfb4ce17439a
Yosub Shin <yosub@yosubshin.com>
2014-08-22 22:20:20 -0500

    Added cassandra.yaml update part in redeploy-node-script.sh

--- scratch/ant-build.sh
+++ scratch/ant-build.sh
@@ -14,39 +14,17 @@ REMOTE_BASE_DIR=/scratch/ISS/shin14/repos
 REMOTE_SCRIPT_DIR=$REMOTE_BASE_DIR/morphous-cassandra-emulab-script
 REMOTE_REDEPLOY_SCRIPT=$REMOTE_SCRIPT_DIR/redeploy-node-script.sh
 
-echo "## Archiving Cassandra source files..."
-tar -czf $CASSANDRA_SRC_TAR_FILE -C $CASSANDRA_SRC_DIR_BASE $CASSANDRA_SRC_DIR_NAME
 
-echo "## Deleting remote host's Cassandra source files"
-ssh -T $SSH_USER@$SSH_URL /bin/bash << EOF
-# Clear out previously existing Cassandra source files
-sudo rm -rf $REMOTE_BASE_DIR/$CASSANDRA_SRC_DIR_NAME
-sudo rm $REMOTE_BASE_DIR/$CASSANDRA_SRC_TAR_FILE
-EOF
-
-echo "## Uploading archived Cassandra source to remote host"
-scp $CASSANDRA_SRC_TAR_FILE $SSH_USER@$SSH_URL:$REMOTE_BASE_DIR
-
-# Clean up tar file
-rm $CASSANDRA_SRC_DIR_BASE/$CASSANDRA_SRC_TAR_FILE
-
-echo "## Executing cluster redeploy script on remote host"
 ssh -T $SSH_USER@$SSH_URL "setenv REMOTE_BASE_DIR $REMOTE_BASE_DIR; setenv CASSANDRA_SRC_TAR_FILE $CASSANDRA_SRC_TAR_FILE; setenv CASSANDRA_SRC_DIR_NAME $CASSANDRA_SRC_DIR_NAME; setenv CLUSTER_SIZE $CLUSTER_SIZE; setenv REMOTE_REDEPLOY_SCRIPT $REMOTE_REDEPLOY_SCRIPT; /bin/bash" << 'EOF'
-# Now in remote machine's shell
-# Set JAVA_HOME to build with Ant
+
 export JAVA_HOME=/usr/lib/jvm/jdk1.7.0
 
-echo "## Unzipping Cassandra source"
-cd $REMOTE_BASE_DIR
-tar -xzf $CASSANDRA_SRC_TAR_FILE
-
-# Clean up tar file
-rm $CASSANDRA_SRC_TAR_FILE
+java -version
 
 cd $REMOTE_BASE_DIR/$CASSANDRA_SRC_DIR_NAME
 
 echo "## Building Cassandra source"
-ant build
+#ant clean build
 
 echo "## Ant Build is over. Invoking redeploy script on remote nodes"
 
@@ -57,4 +35,3 @@ echo "## Invoking redeploy script on node-$i"
 ssh -T -o "StrictHostKeyChecking no" node-$i "sudo $REMOTE_REDEPLOY_SCRIPT node-$i"
 done
 
-'EOF'
