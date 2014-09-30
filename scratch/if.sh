6f9068594dc8b58d6a415efc2db52aedb9c3b034
Yosub Shin <yosub@yosubshin.com>
2014-09-30 09:20:05 -0500

    Cleaned up

--- scratch/if.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-NODE_ADDRESS=$1
-
-if [ $NODE_ADDRESS == "node-0" ]; then
-    echo "## Executing deploy Cassandra script..."
-    /scratch/ISS/shin14/repos/morphous-cassandra-emulab-script/deploy-cassandra-cluster.sh
-fi
